4f500fc6c712fd6ca6d723436b1005221796a64c
Clucas0311 <Clucas0311@gmail.com>
2022-10-17 13:41:20 -0400

    feat(fetch all post from api)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BASE_URL } from "./api";
+import { Posts, Loading } from "./components";
 
 const App = () => {
-  return <div>This is our first App</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/posts`);
+        const { data } = await response.json();
+        setPosts(data.posts);
+      } catch (error) {
+        console.error("There was an error fetching posts", error);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <Posts posts={posts} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  "https://strangers-things.herokuapp.com/api/2207-FTB-ET-WEB-PT";
